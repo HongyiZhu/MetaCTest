399ba4ff37b76c0dd70b0f1b80c86003f62663f6
HongyiZhu <hongyi.zhu.21@gmail.com>
2016-08-24 16:42:50 -0700

    slightly modified the UI broadcast

--- app/src/main/java/com/example/hongyi/foregroundtest/BodyBoard.java
+++ app/src/main/java/com/example/hongyi/foregroundtest/BodyBoard.java
@@ -176,6 +176,7 @@ public class BodyBoard extends Board{
                                         }
                                     }
                                 }
+                                broadcastStatus();
                             }
                         };
 
